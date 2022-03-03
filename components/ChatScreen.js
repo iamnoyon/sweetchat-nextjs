ec45fecbd30b9404dd57697d4d2026f1a7ee6a3b
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2022-03-04 03:45:40 +0600

    chatpage design

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,35 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import MoreVert from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
     return (
         <Container>
-            <h1> This is a chat</h1>
+            <Header>
+                <Avatar/>
+                <HeaderInformation>
+                    <h3>Receipient Email</h3>
+                    <p>Last Seen...</p>
+                </HeaderInformation>
+                <HeaderIcons>
+                <IconButton>
+                        <AttachFileIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </HeaderIcons>
+            </Header>
+            <MessageContainer>
+                {/*show messages*/}
+                <EndOfMessage/>
+            </MessageContainer>
         </Container>
     )
 }
@@ -11,4 +37,33 @@ function ChatScreen() {
 export default ChatScreen
 
 const Container = styled.div`
+`;
+const Header = styled.div`
+    position: sticky;
+    background-color: white;
+    z-index: 100;
+    top: 0;
+    display: flex;
+    padding: 11px;
+    height: 80px;
+    align-items: center;
+    border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div`
+    margin-left: 15px;
+    flex: 1;
+
+    > h3{
+        margin-bottom: 3px; 
+    }
+    > p{
+        font-size: 14px;
+        color: gray;
+    }
+`;
+const HeaderIcons = styled.div`
+`;
+const MessageContainer = styled.div`
+`;
+const EndOfMessage = styled.div`
 `;
